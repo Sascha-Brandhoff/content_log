b7f83c30cd9a6b7328eeb1bcd934c34070c49802
root <root@static.148-251-70-116.clients.your-server.de>
2016-04-18 08:28:16 +0200

    added copyright information

--- config/config.php
+++ config/config.php
@@ -1,8 +1,23 @@
 <?php
 
+/**
+ * Frontend-Modules
+ **/
 array_insert($GLOBALS['FE_MOD']['miscellaneous'], 0, array
 (
 	'content_log'        => 'ModuleContentLog'
 ));
 
+/**
+ * Hooks
+ **/
 $GLOBALS['TL_HOOKS']['contentLog'][] = array('ClassContentLog', 'checkLog');
+
+/**
+ * Copyright information
+ */
+$GLOBALS['TL_HEAD']['PIXELSPREADDE'] = '<!--
+    This Contao OpenSource CMS uses modules from pixelSpread.de
+    Copyright (c)2012 - 2014 by Sascha Brandhoff :: Extensions of pixelSpread.de are copyright of their respective owners
+    Visit our website at http://www.pixelSpread.de for more information
+//-->';
